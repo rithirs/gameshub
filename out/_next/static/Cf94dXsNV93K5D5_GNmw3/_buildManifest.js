8aee7afbb982e4d19f85547206ba7bb33c691d2c
R Rithika Sharon <208923760+rithirs@users.noreply.github.com>
2025-08-01 23:49:00 +0530

    Remove unused static files and HTML pages for the GamesHub project, including webpack and CSS files, as well as index.html and Tic-Tac-Toe page.

--- out/_next/static/Cf94dXsNV93K5D5_GNmw3/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-d375bf1ac0ec8648.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],"/tic-tac-toe":["static/chunks/pages/tic-tac-toe-996324ed45a340ff.js"],sortedPages:["/","/_app","/_error","/tic-tac-toe"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
